30f8843bd9e8d334a022c5e9b3548bcb9c1a830c
Antonio <tonioufa@gmail.com>
2024-09-30 10:12:13 +0100

    adds erc20 contract

---
+++ contracts/ERC20.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20 is ERC20, Ownable {
+    constructor(
+        string name,
+        string symbol,
+        address initialOwner
+    ) ERC20(name, symbol) Ownable(initialOwner) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
